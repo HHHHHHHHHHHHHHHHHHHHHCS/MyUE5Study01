641b2066c925faa5bfc2938a4a0ca512dd626727
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2023-02-15 20:46:26 +0800

    add

--- Source/MyUE5Study01/Private/MyPickupActor.cpp
+++ Source/MyUE5Study01/Private/MyPickupActor.cpp
@@ -11,9 +11,14 @@ AMyPickupActor::AMyPickupActor()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	sphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	sphereComp->SetSphereRadius(80.0f);
 	SetRootComponent(sphereComp);
+
 	decalComp = CreateDefaultSubobject<UDecalComponent>(TEXT("DecalComp"));
 	decalComp->SetupAttachment(sphereComp);
+	decalComp->SetRelativeLocation(FVector(0, 0, 20));
+	decalComp->SetRelativeRotation(FRotator(90, 0, 0));
+	decalComp->DecalSize = FVector(40, 80, 80);
 }
 
 // Called when the game starts or when spawned
